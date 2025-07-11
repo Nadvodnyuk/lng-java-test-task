cfab952e3a9f2778cd8a87d089657286ec3e130e
ynadvodnyuk <rimsky.yana@yandex.com>
2025-07-12 00:15:10 +0300

    file reading and writing, line parsing, README

---
+++ src/main/java/org/task/LineParser.java
@@ -0,0 +1,25 @@
+package org.task;
+
+import java.util.Arrays;
+
+public class LineParser {
+    public static String[] parseLineOrReturnNull(String line) {
+        if (line == null || line.isEmpty()){
+            return null;
+        }
+        String[] cols = line.split(";", -1);
+        for (String col : cols){
+            if (!(col.length() >= 2 && col.startsWith("\"") && col.endsWith("\""))){
+                return null;
+            }
+            String inner = col.substring(1, col.length() - 1);
+            if (inner.indexOf('"') >= 0) {
+                return null;
+            }
+        }
+        cols = Arrays.stream(cols)
+                .map(col -> col.substring(1, col.length() - 1))
+                .toArray(String[]::new);
+        return cols;
+    }
+}
--- src/main/java/org/task/Main.java
+++ src/main/java/org/task/Main.java
@@ -1,7 +1,38 @@
 package org.task;
 
+import java.io.*;
+import static org.task.LineParser.parseLineOrReturnNull;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws IOException {
+        long start = System.nanoTime();
+        if (args.length == 0) {
+            System.err.println("Ошибка: не передан путь к файлу.");
+            System.exit(1);
+        }
+        String pathToFile = args[0];
+
+        String line;
+        int count = 0;
+
+        try (
+                BufferedReader reader = new BufferedReader(new FileReader(pathToFile))
+        ) {
+            while ((line = reader.readLine()) != null) {
+                if (parseLineOrReturnNull(line) != null){
+                    count += 1;
+                }
+            }
+        }
+
+        try (
+                BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))
+        ) {
+            writer.write("Количество строк: " + count);
+        }
+        long end = System.nanoTime();
+        long elapsedNs = end - start;
+        System.out.printf("Время выполнения: %.3f мс%n", elapsedNs / 1_000_000_000.0);
     }
 }
